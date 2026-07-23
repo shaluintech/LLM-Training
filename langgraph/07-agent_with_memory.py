04bb7cee68580a5486086bfd968222c2b2bf51c4
shaluintech <shalugupta15078@gmail.com>
2026-07-23 14:11:27 +0530

    Add agent with memory

---
+++ langgraph/07-agent_with_memory.py
@@ -0,0 +1,30 @@
+import os
+from typing import List
+
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_core.tools import tool
+from langchain_core.messages import AIMessage
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.outputs import ChatResult, ChatGeneration
+from langgraph.checkpoint.memory import MemorySaver
+
+load_dotenv()
+MODEL = "llama-3.1-8b-instant"
+
+
+@tool
+def add(a: int, b: int) -> int:
+    """Add two integers a and b and return the sum."""
+    return a + b
+if os.getenv("GROQ_API_KEY"):
+    from langchain_groq import ChatGroq
+    model = ChatGroq(model=MODEL,temperature=0)
+    agent = create_agent(model,[add],checkpointer=MemorySaver())
+    config = {"configurable":{"thread_id":"Student-1"}}
+    response1 = agent.invoke({"messages":[("human", "Hii my name is Shalu")]},config)
+    print(response1['messages'][-1].content)
+    response2 = agent.invoke({"messages":[("human", "What is my name")]},config)
+    print(response2['messages'][-1].content)
+else:
+    print("NO API KEY")
